e04b1d3a74515e3bb140823ec4e2cead91bebc9f
Gabriel Paez <gabpaez@gmail.com>
2020-03-11 12:21:32 -0300

    Timer y Carga de datos estaticos para probar

---
+++ Timer.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+import { millisecondsToHuman } from "./utils/timerUtils";
+import TimerButton from "./TimerButton";
+
+const Timer = ({ title, project, elapsed }) => {
+  
+    const elapsedString = millisecondsToHuman(elapsed);
+
+  return (
+    <View style={styles.timerContainer}>
+          <Text style={styles.title}>{title}</Text>
+      <Text>{project}</Text>
+      <Text style={styles.elapsedTime}>{elapsedString}</Text>
+      <View style={styles.buttonGroup}>
+        <TimerButton color="blue" small title="Edit" />
+        <TimerButton color="blue" small title="Remove" />
+      </View>
+      <TimerButton color="#21BA45" title="Start" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  timerContainer: {
+    backgroundColor: "white",
+    borderColor: "#D6D7DA",
+    borderWidth: 2,
+    borderRadius: 10,
+    padding: 15,
+    margin: 15,
+    marginBottom: 0
+  },
+  title: {
+    fontSize: 14,
+    fontWeight: "bold"
+  },
+  elapsedTime: {
+    fontSize: 26,
+    fontWeight: "bold",
+    textAlign: "center",
+    paddingVertical: 15
+  },
+  buttonGroup: {
+    flexDirection: "row",
+    justifyContent: "space-between"
+  }
+});
+
+export default Timer;
--- components/EditableTimer.js
+++ components/EditableTimer.js
@@ -1,16 +1,29 @@
-import React from 'react';
-import { View, Text, StyleSheet} from 'react-native';
+import React from "react";
 
-const EditableTimer = () => {
-    return ( 
-        <View>
-            <Text>
-                EditableTimer Page
-            </Text>
-        </View>
-     );
-}
+import TimerForm from "../TimerForm";
+import Timer from "../Timer";
 
-const styles = StyleSheet.create({})
- 
+const EditableTimer = ({
+  id,
+  title,
+  project,
+  elapsed,
+  isRunning,
+  editFormOpen
+}) => {
+  if (editFormOpen) {
+    return <TimerForm id={id} title={title} project={project} />;
+  }
+
+  return (
+    <Timer
+      id={id}
+      title={title}
+      project={project}
+      elapsed={elapsed}
+      isRunning={isRunning}
+    />
+  );
+};
+
 export default EditableTimer;
